af896fa6199d5c777812ca95429693a244574e4e
Simon <simonmarkholland@gmail.com>
2018-11-26 15:18:42 +0700

    Annotate file

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ mongoose        = require("mongoose"),
 express         = require("express"),
 app             = express();
 
-mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser : true});
+// App Config
 
+mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser : true});
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
+
+// Mongoose model configuration
+// Restful Routes
 
 var listener = app.listen(3000, "127.0.0.1", function(){
   var address = listener.address().address;
