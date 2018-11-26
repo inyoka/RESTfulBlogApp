abd82d7ab0bb4d06777109a64f95a27954243fbd
Simon <simonmarkholland@gmail.com>
2018-11-26 15:09:45 +0700

    Connect to mongoose database.

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ var bodyparser  = require("body-parser"),
 mongoose        = require("mongoose"),
 express         = require("express"),
 app             = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser : true});
