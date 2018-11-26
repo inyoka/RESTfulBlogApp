87491b23fbb2936a1634216ed9fb390bde720f6c
Simon <simonmarkholland@gmail.com>
2018-11-26 15:11:48 +0700

    Setup the server to listen on localhost port 3000.

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser : true
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyparser.urlencoded({extended:true}));
+
+var listener = app.listen(3000, "127.0.0.1", function(){
+  var address = listener.address().address;
+  var port = listener.address().port;
+
+  console.log("Server running on : " +address+":"+port);
+});
