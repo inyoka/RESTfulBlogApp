ca349947aaf08f675de09b62b6649ed12f871a3e
Simon <simonmarkholland@gmail.com>
2018-11-26 15:10:45 +0700

    Setup view engine, static public directory and bodyparser.

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ express         = require("express"),
 app             = express();
 
 mongoose.connect("mongodb://localhost/restful_blog_app", {useNewUrlParser : true});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({extended:true}));
